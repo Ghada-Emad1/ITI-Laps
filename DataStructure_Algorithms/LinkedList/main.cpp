805709ee58ee19f2d7f5ba6201fcb5920d4bf401
Ghada Emad <ghadaemad011@gmail.com>
2024-11-14 13:47:14 +0200

    adding more functionality to menu and update LinkedList

--- DataStructure_Algorithms/LinkedList/main.cpp
+++ DataStructure_Algorithms/LinkedList/main.cpp
@@ -8,11 +8,23 @@ struct Node {
     int age;
     Node *next;
     Node *prev;
+
 };
 
 class LinkedList {
     Node *head;
     Node *tail;
+    Node*SearchByIdPriv(int id){
+        Node*temp=head;
+        while(temp!=NULL){
+            if(temp->id==id){
+                return temp;
+            }else{
+                temp=temp->next;
+            }
+        }
+        return NULL;
+    }
 
 public:
     LinkedList() {
@@ -42,12 +54,52 @@ public:
         }
     }
 
+    int counting(){
+        int cnt=0;
+        Node*temp=head;
+        while(temp!=NULL){
+            cnt++;
+            temp=temp->next;
+        }
+        return cnt;
+    }
+    Node SearchById(int id){
+        Node*resNode=SearchByIdPriv(id);
+        Node res;
+        if(resNode!=NULL){
+            res.age=resNode->age;
+            res.id=resNode->id;
+            strcpy(res.Name,resNode->Name);
+            res.prev=NULL;
+            res.next=NULL;
+        }else{
+            res.id=-1;
+        }
+        return res;
+    }
+    ~LinkedList(){
+        Node*temp=head;
+        while(temp!=NULL){
+            head=temp->next;
+
+            delete temp;
+            temp=head;
+        }
+    }
+
 
 };
 
 int main() {
     LinkedList ll;
     ll.append(1, "ghada", 22);
+    ll.append(3,"ali",88);
     ll.Display();
+
+    //cout<<ll.counting();
+    Node res=ll.SearchById(1);
+    if(res.id!=-1){
+        cout<<res.id<<" " <<res.Name<< " "<<res.age;
+    }
     return 0;
 }
--- DataStructure_Algorithms/Menu/main.cpp
+++ DataStructure_Algorithms/Menu/main.cpp
@@ -14,14 +14,48 @@ struct Node {
 class EmpList {
     Node *head;
     Node *tail;
+    Node *SearchByIdPriv(int id){
+        Node*temp=head;
+        while(temp!=NULL){
+            if(temp->id==id){
+                return temp;
+            }else{
+                temp=temp->next;
+            }
+        }
+        return NULL;
+    }
+    Node*SearchByNamePriv(char*Name){
+        Node*temp=head;
+        while(temp!=NULL){
+            if(strcmp(temp->Name,Name)==0){
+                return temp;
+            }else{
+            temp=temp->next;
+            }
+        }
+        return NULL;
+    }
 
 public:
-    EmpList() : head(NULL), tail(NULL) {}
+    EmpList() {
+        head=tail=NULL;
+    }
+    EmpList(EmpList&e){
+        head=tail=NULL;
+        Node*temp=e.head;
+        while(temp!=NULL){
+            AddEmp(temp->id,temp->Name);
+            temp=temp->next;
+        }
+        cout<<"deep copy constructor"<<endl;
+    }
 
     void AddEmp(int id, const char *Name) {
         Node *temp = new Node();
         temp->id = id;
         strcpy(temp->Name, Name);
+
 
 
         if (head == NULL) {
@@ -34,24 +68,150 @@ public:
     }
 
     void Display() {
-        if (head == NULL) {
-            cout << "No Employees Data to display\n";
+        Node *temp = head;
+        if(temp==NULL){
+            cout<<"There are No Employees "<<endl;
             return;
         }
-
-        Node *temp = head;
         while (temp != NULL) {
             cout << "Employee Id: " << temp->id << ", Employee Name: " << temp->Name << endl;
             temp = temp->next;
         }
     }
 
-    ~EmpList() { // Destructor to free memory
-        Node *current = head;
-        while (current) {
-            Node *nextNode = current->next;
-            delete current;
-            current = nextNode;
+    int CountEmp(){
+        int cnt=0;
+        Node*temp=head;
+        while(temp!=NULL){
+            cnt++;
+            temp=temp->next;
+        }
+        return cnt;
+    }
+
+    Node SearchById(int id){
+        Node *resAddr=SearchByIdPriv(id);
+        Node res;
+        if(resAddr!=NULL){
+            res.id=resAddr->id;
+            strcpy(res.Name,resAddr->Name);
+            res.next=NULL;
+            res.prev=NULL;
+
+        }else{
+            res.id=-1;
+        }
+        return res;
+
+    }
+    Node SearchByName(char*Name){
+        Node*resAddr=SearchByNamePriv(Name);
+        Node res;
+        if(resAddr!=NULL){
+            res.id=resAddr->id;
+            strcpy(res.Name,resAddr->Name);
+            res.next=NULL;
+            res.prev=NULL;
+        }else{
+            res.id=-1;
+        }
+        return res;
+    }
+    void DeleteById(int id){
+        Node*res=SearchByIdPriv(id);
+        if(res==NULL){
+            return;
+        }
+        if(head==tail){
+            head=tail=NULL;
+        }
+        else if(res==head){
+            head=res->next;
+            res->prev=NULL;
+        }else if(res==tail){
+            tail=tail->prev;
+            tail->next=NULL;
+        }else{
+            res->prev->next=res->next;
+            res->next->prev=res->prev;
+        }
+    }
+    void DeleteByName(char *Name){
+        Node *res=SearchByNamePriv(Name);
+        if(res==NULL){
+            return;
+        }
+        if(head==tail){
+            head=tail=NULL;
+        }
+        else if(res==head){
+            head=res->next;
+            res->prev=NULL;
+        }else if(res==tail){
+            tail=tail->prev;
+            tail->next=NULL;
+        }else{
+            res->prev->next=res->next;
+            res->next->prev=res->prev;
+        }
+
+    }
+    void insertById(int id,char*Name,int ExistingId){
+        if(head==NULL){
+            AddEmp(id,Name);
+            return;
+        }
+        Node*res=SearchByIdPriv(ExistingId);
+        if(res==tail){
+            AddEmp(id,Name);
+        }else{
+            Node*temp=new Node();
+            temp->id=id;
+            strcpy(temp->Name,Name);
+            temp->prev=res->prev;
+            temp->next=res;
+            if(res->prev!=NULL){
+                res->prev->next=temp;
+            }else{
+                head=temp;
+            }
+            res->prev=temp;
+        }
+    }
+    void UpDate(int id,char*Name,int ExistingId){
+        Node*res=SearchByIdPriv(ExistingId);
+        if(res==NULL){
+            cout<<"Employee with Id "<<ExistingId<<" Not Found"<<endl;
+            return;
+        }
+        else{
+            res->id=id;
+            strcpy(res->Name,Name);
+        }
+
+    }
+    EmpList& operator=(const EmpList& e){
+        Node*temp=head;
+        while(temp!=NULL){
+            head=temp->next;
+            delete temp;
+            temp=head;
+        }
+        head=tail=NULL;
+        Node*current=e.head;
+        while(current!=NULL){
+            AddEmp(current->id,current->Name);
+            current=current->next;
+        }
+        return *this;
+    }
+    ~EmpList() {
+        Node *temp = head;
+        while (temp!=NULL) {
+           head=temp->next;
+           delete temp;
+           temp=head;
+
         }
     }
 };
@@ -68,14 +228,17 @@ void gotoxy(int column, int line) {
 }
 
 int main() {
-    char menu[6][40] = {"New", "Display", "Display_id", "Display_Name", "Update", "Exit"};
+    char menu[11][40] = {"New", "Display","Number_of_Employee" ,"Search By Id","Search By Name", "InsertById", "DeleteById","DeleteByName","Updating","Display_Another_One","Exit"};
     int n = 0, flag = 0;
     EmpList Emp;
+    EmpList Emp2;
+
+    Node res;
     char ch;
 
     do {
         system("cls");
-        for (int i = 0; i < 6; i++) {
+        for (int i = 0; i < 11; i++) {
             gotoxy(10, 10 + i);
             if (i == n) {
                 textattr(0x02);
@@ -92,11 +255,11 @@ int main() {
             switch (ch) {
                case 72:
                     n--;
-                    if (n < 0) n =5 ;
+                    if (n < 0) n = 10;
                     break;
                 case 80:
                     n++;
-                    if (n > 5) n = 0;
+                    if (n > 10) n = 0;
                     break;
             }
         }
@@ -115,8 +278,7 @@ int main() {
                         cout << "Enter Employee Id: ";
                         cin >> empId;
                         cout << "Enter Employee Name: ";
-                        cin.ignore();
-                        cin.getline(empName, 30);
+                        cin>>empName;
                         Emp.AddEmp(empId, empName);
                     }
                     break;
@@ -124,11 +286,75 @@ int main() {
                 case 1:
                     Emp.Display();
                     break;
+                case 2 :
+                    cout<<"Number of Employees is : "<<Emp.CountEmp();
+                    break;
+                case 3:
+                    int id;
+                    cout<<"Enter Id of Employee you are looking for : ";
+                    cin>>id;
+                    res=Emp.SearchById(id);
+                    if(res.id!=-1){
+                         cout << "Employee Id: " << res.id << ", Employee Name: " <<res.Name << endl;
 
+                    }
+                    break;
+                case 4:
+                    char Name[30];
+                    cout<<"Enter Name of Employee you are Looking for : ";
+                    cin>>Name;
+                    res=Emp.SearchByName(Name);
+                    if (res.id!=-1){
+                        cout << "Employee Id: " << res.id << ", Employee Name: " <<res.Name << endl;
+
+                    }
+                    break;
                 case 5:
+
+                    int New_Id,ExistingId;
+                    char New_Name[30];
+                    cout<<"Enter Existing Id";
+                    cin>>ExistingId;
+                    cout<<"Enter Id : ";
+                    cin>>New_Id;
+                    cout<<"Enter Name : ";
+                    cin>>New_Name;
+                    Emp.insertById(New_Id,New_Name,ExistingId);
+                    break;
+                case 6:
+                    int Deleted_id;
+                    cout<<"Enter ID of Employee You want to Delete : ";
+                    cin>>Deleted_id;
+                    Emp.DeleteById(Deleted_id);
+                    break;
+                case 7:
+                    char Deleted_Name[30];
+                    cout<<"Enter Name of Employee You want to Delete : ";
+                    cin>>Deleted_Name;
+                    Emp.DeleteByName(Deleted_Name);
+                    break;
+                case 8:
+                    int Update_id;
+                    char Update_Name[30];
+                    int ExistingId1;
+                    cout<<"Enter Id of Employee You Want to Update : ";
+                    cin>>ExistingId1;
+                    cout<<"Enter New Id : ";
+                    cin>>Update_id;
+                    cout<<"Enter New Name : ";
+                    cin>>Update_Name;
+                    Emp.UpDate(Update_id,Update_Name,ExistingId1);
+                    break;
+                case 9:
+                    EmpList Emp2;
+                    Emp2=Emp;
+                    Emp2.Display();
+                    break;
+
+                /*case 5:
                     cout << "You selected: Exit\n";
                     flag = 1;
-                    break;
+                    break;*/
             }
             _getch();
         }
